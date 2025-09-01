6525a8af77e12d772a1b248e2da1713f71a0e211
Fatih Ünlü <unlu.fa@gmail.com>
2025-09-01 19:46:17 +0300

    add opentelemetry into product-api

--- services/product-api/src/Program.cs
+++ services/product-api/src/Program.cs
@@ -1,4 +1,43 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// --- OpenTelemetry config ---
+var svcName = Environment.GetEnvironmentVariable("SERVICE_NAME") ?? "ecom.product.api";
+var svcVersion = Environment.GetEnvironmentVariable("SERVICE_VERSION") ?? "0.1.0";
+var environment = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "local";
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(r => r
+        .AddService(serviceName: svcName, serviceVersion: svcVersion)
+        .AddAttributes(new KeyValuePair<string, object>[]
+        {
+            new("deployment.environment", environment)
+        }))
+    .WithTracing(t => t
+        .AddAspNetCoreInstrumentation(opt =>
+        {
+            opt.RecordException = true;
+            opt.EnrichWithHttpRequest = (activity, request) =>
+            {
+                activity.SetTag("http.request_content_length", request.ContentLength);
+            };
+            opt.EnrichWithHttpResponse = (activity, response) =>
+            {
+                activity.SetTag("http.response_content_length", response.ContentLength);
+            };
+        })
+        .AddHttpClientInstrumentation(opt => opt.RecordException = true)
+        .AddOtlpExporter()
+    )
+    .WithMetrics(m => m
+        .AddRuntimeInstrumentation()
+        .AddMeter("Microsoft.AspNetCore")
+        .AddOtlpExporter()
+    );
+
 var app = builder.Build();
 
 var port = Environment.GetEnvironmentVariable("HTTP_PORT") ?? "5001";
